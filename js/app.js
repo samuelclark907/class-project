100af33a63b0d7a7d5b61f41f6502ace4418094b
sam <samuelclark907@gmail.com>
2020-05-06 18:13:41 -0700

    add loop

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function offerGreeting(){
+function offerGreeting() {
 var today = new Date(); 
 var hourNow = today.getHours(); 
 var greeting; 
@@ -15,19 +15,46 @@ if (hourNow > 18) {
 return document.write('<h3>' + greeting + '</h3>'); 
 }
 
-function askName(){
+function askName() {
     var userName = prompt('What is your name?');
-    return document.write('<h3>' + 'Nice to meet you' + userName + '</h3>');
+    return document.write('<h3>' + 'Nice to meet you ' + userName + '</h3>');
+
+
 }
 
-function askWashhands(){
-    var washhands = confirm('This site might be infected with CV');
-    var txt;
-    if (washhands === true)  {
-        txt = "Use some hand sanitizer";
-      } else {
-        txt = "Im just a website I cant have CV";
-      }
-    
-    return document.write(txt);
+
+function getProduct() {
+    var response = prompt('Would you like to buy some Corona Virus?');
+    var item;
 }
+    while (response !== 'yes' && response !== 'no') {
+        response = prompt('Yes or No question. Do you want some CV?');
+      }
+
+      if (response === 'yes') {
+          item = '<img src ="class-project-corona.jpg">';
+      } else if (response === 'no') {
+          response = prompt('Are you sure? It/s some good CV.');
+      }
+      return item;
+
+     
+      
+      function getCount() {
+          var count = prompt('Perfect, how many would you like?');
+          
+      } while (isNaN(count) || count === '') {
+          count = prompt('Put in a number, how many CV/s you want?');
+      }
+        return count;
+      
+    
+      function showOrder() {
+          var result = '';
+          var itemType = getProduct();
+          var total = getCount();
+      }
+          for (var i = 0; i < total; i++) {
+            result = result +'<p>' + itemType + '</p>'
+          }
+      return document.write(result)
