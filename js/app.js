6c1660866e2423764c67196c682deb908ffa23ba
sam <samuelclark907@gmail.com>
2020-05-06 18:49:58 -0700

    fixed app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 function offerGreeting() {
-var today = new Date(); 
-var hourNow = today.getHours(); 
-var greeting; 
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greeting;
 
-if (hourNow > 18) { 
-    greeting = 'Good evening!';
-}else if (hourNow > 12) { 
-    greeting = 'Good afternoon!';
-}else if (hourNow > 0) { 
-    greeting = 'Good morning!';
-}else { 
-    greeting = 'Welcome!'; 
-}
-return document.write('<h3>' + greeting + '</h3>'); 
+    if (hourNow > 18) {
+        greeting = 'Good evening!';
+    } else if (hourNow > 12) {
+        greeting = 'Good afternoon!';
+    } else if (hourNow > 0) {
+        greeting = 'Good morning!';
+    } else {
+        greeting = 'Welcome!';
+    }
+
+    return document.write('<h3>' + greeting + '</h3>');
 }
 
 function askName() {
@@ -26,35 +27,37 @@ function askName() {
 function getProduct() {
     var response = prompt('Would you like to buy some Corona Virus?');
     var item;
-}
+
     while (response !== 'yes' && response !== 'no') {
         response = prompt('Yes or No question. Do you want some CV?');
-      }
+    }
 
-      if (response === 'yes') {
-          item = '<img src ="class-project-corona.jpg">';
-      } else if (response === 'no') {
-          response = prompt('Are you sure? It/s some good CV.');
-      }
-      return item;
+    if (response === 'yes') {
+        item = '<img src ="/img/class-project-corona.jpg">';
+    } else if (response === 'no') {
+        response = prompt('Are you sure? It/s some good CV.');
+    }
+    return item;
 
-     
-      
-      function getCount() {
-          var count = prompt('Perfect, how many would you like?');
-          
-      } while (isNaN(count) || count === '') {
-          count = prompt('Put in a number, how many CV/s you want?');
-      }
-        return count;
-      
-    
-      function showOrder() {
-          var result = '';
-          var itemType = getProduct();
-          var total = getCount();
-      }
-          for (var i = 0; i < total; i++) {
-            result = result +'<p>' + itemType + '</p>'
-          }
-      return document.write(result)
+}
+
+
+function getCount() {
+    var count = prompt('Perfect, how many would you like?');
+
+    while (isNaN(count) || count === '') {
+        count = prompt('Put in a number, how many CV/s you want?');
+    }
+    return count;
+}
+
+function showOrder() {
+    var result = '';
+    var itemType = getProduct();
+    var total = getCount();
+
+    for (var i = 0; i < total; i++) {
+        result = result + '<p>' + itemType + '</p>'
+    }
+    return document.write(result)
+}
